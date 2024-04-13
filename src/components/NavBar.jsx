75a2d2ba64a20968b71bbc404432ed4e156e7509
Ahmed-Mohamed-Egypt <ahmedatyabas@gmail.com>
2024-04-13 22:03:03 +0200

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,56 +1,24 @@
 import { NavLink, Link, useNavigate } from "react-router-dom";
 import { UseTranslate } from "../context/TranslateContext";
 import { Fragment } from "react";
+import SubList from "../hooks/SubList";
 
 function NavBar() {
-  const { dataLang, show, setShow, setActivateCategory } = UseTranslate();
+  const { dataLang, setShow } = UseTranslate();
 
   const allList = Object.keys(dataLang.maindocs).map((key) => {
     return { ...dataLang.maindocs[key][0] };
   });
 
-  const Navigate = useNavigate();
-  const { subTitlesNav } = { ...dataLang.maindocs.roadmap }[0];
+  
+  const { subTitlesNav:subListRoadMap } = { ...dataLang.maindocs.roadmap }[0];
 
   return (
     <nav className="mrGain-navBar">
       {allList.map((item, index) => (
         <Fragment key={index}>
           {index === 4 ? (
-            <div>
-              <p
-                onClick={() => {
-                  {
-                    setShow(show === index ? "" : index);
-                    Navigate(`${item.path}`);
-                    setActivateCategory(false);
-                  }
-                }}
-              >
-                <span className="mainLink">{item.titleNavBar}</span>
-                <i
-                  className={`${
-                    show === index ? "rotate" : ""
-                  } chevronDown bi bi-chevron-right`}
-                ></i>
-              </p>
-              {show === index && (
-                <ul className="mtop-15">
-                  {subTitlesNav.map((itemSub,index) => {
-                    return (
-                      <li className="mrb-15" key={index}>
-                        <NavLink
-                          onClick={() => setActivateCategory(true)}
-                          to={`${itemSub.path}`}
-                        >
-                          {itemSub.title}
-                        </NavLink>
-                      </li>
-                    );
-                  })}
-                </ul>
-              )}
-            </div>
+            <SubList indexItem={index} subList={subListRoadMap} mainItem={item}/>
           ) : (
             <NavLink onClick={() => setShow(index)} to={item.path}>
               {item.titleNavBar}
