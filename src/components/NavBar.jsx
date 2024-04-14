510124908d2705fe9461060dcd55a7c957d6fb3a
Ahmed-Mohamed-Egypt <ahmedatyabas@gmail.com>
2024-04-14 14:51:39 +0200

    update-close-menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { NavLink, Link, useNavigate } from "react-router-dom";
 import { UseTranslate } from "../context/TranslateContext";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import SubList from "../hooks/SubList";
 
 function NavBar() {
@@ -12,8 +12,18 @@ function NavBar() {
 
   const allSubTitlesNav = allList.map((item) => item.subTitlesNav);
 
+  const [close,setClose] = useState(false)
+  if(close){
+    document.body.classList.add('close')
+  }else {
+    document.body.classList.remove('close')
+  }
+
   return (
     <nav className="mrGain-navBar">
+      <button className="btn closeNav" onClick={()=>setClose(close=>!close)}>
+      <i className="bi bi-arrow-left-short"></i>
+      </button>
       {allList.map((item, index) => (
         <Fragment key={index}>
           {
